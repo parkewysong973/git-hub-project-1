832edf9c83c92e94822b54a8873c0b0cd84756f6
parkewysong973 <parkewysong973@gmail.com>
2025-04-06 09:21:43 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_data():
+    # This function should contain the data loading logic.
+    pass
+
+# Call the function to get data
+data = load_data()
